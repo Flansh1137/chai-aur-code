36b34dd6b99d0b443123b285fd4566a93af2bac0
Flansh Gajbhiye <rickygajbhiye1137@gmail.com>
2025-01-30 22:10:38 +0530

    after login corrected vercel.json and viteconfig file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  optimizeDeps: {
+    include: ["react-datepicker"],
+  },
   plugins: [react()],
   build: {
     outDir: 'dist', // Specify output directory
