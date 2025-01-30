860f6a3852517c5ff84243e48239ba461d571e31
Flansh Gajbhiye <rickygajbhiye1137@gmail.com>
2025-01-30 13:15:56 +0530

    auth0

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
-
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 const root = createRoot(document.getElementById('root'));
 
 // Define the redirect after login dynamically
@@ -12,8 +13,8 @@ const onRedirectCallback = (appState) => {
 
 root.render(
   <Auth0Provider
-    domain="dev-dedaezfkps23cxcu.us.auth0.com"
-    clientId="KexQt69RUglMtDtcTLm5P1tbtk0YTUUd"
+  domain={domain}
+  clientId={clientId}
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
